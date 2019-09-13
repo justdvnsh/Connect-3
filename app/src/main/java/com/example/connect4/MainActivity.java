bc95288a21e8b3fb45143df2137bc30c4d8213f7
justdvnsh <justdvnsh2208@gmail.com>
2019-09-13 12:28:12 +0530

    added grid layout

--- app/src/main/java/com/example/connect4/MainActivity.java
+++ app/src/main/java/com/example/connect4/MainActivity.java
@@ -8,35 +8,9 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void fade(View view) {
-
-        ImageView img1 = (ImageView) findViewById(R.id.img1);
-        ImageView img2 = (ImageView) findViewById(R.id.img2);
-
-        img1.animate().translationYBy(1000f).setDuration(2000);
-
-        img2.animate().translationXBy(-1000f).setDuration(2000);
-
-    }
-
-    public void fadeAgain(View view) {
-
-        ImageView img1 = (ImageView) findViewById(R.id.img1);
-        ImageView img2 = (ImageView) findViewById(R.id.img2);
-
-        img2.animate().alpha(0f).setDuration(2000);
-
-        img1.animate().alpha(1f).setDuration(2000);
-
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ImageView img2 = (ImageView) findViewById(R.id.img2);
-
-        img2.setTranslationX(1000f);
     }
 }
